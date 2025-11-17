75052039d2990ccdeccfba2f866cfc5a3c485bd6
Arthur Souza <arthurecassio@gmail.com>
2025-11-17 07:52:25 -0300

    Atualizando Aula de Hash

---
+++ Aula15-Hash/Aluno.py
@@ -0,0 +1,9 @@
+class Aluno():
+    def __init__(self, matricula, nome):
+        self.matricula = matricula
+        self.nome = nome
+
+class Brasileiro():
+    def __init__(self, cpf, nome):
+        self.cpf = cpf
+        self.nome = nome
