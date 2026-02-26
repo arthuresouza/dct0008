0989899f3d6ae44b79897892fee5fff8db1478cf
Arthur Souza <arthurecassio@gmail.com>
2026-02-26 09:19:39 -0300

    Organizando as Branches das Unidades

--- Aula01-Revisão/Atividade01.py
+++
@@ -1,8 +0,0 @@
-
-def cadastrarUsuario():
-    nome = input("informe o nome: ")
-    email = input("informe o email: ")
-    print("O usuário foi cadastrado com sucesso")
-
-if __name__ == "__main__":
-    cadastrarUsuario()
