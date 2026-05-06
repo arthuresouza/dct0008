cf9a80514010f1b1aa95fc1b3e13f44427a3fa7f
Arthur Souza <arthur.souza@ufrn.br>
2026-05-06 09:36:38 -0300

    Corrigindo QuickSort para o método

--- Aula07-Busca-Ordenação/QuickSort.py
+++ Aula07-Busca-Ordenação/QuickSort.py
@@ -1,5 +1,5 @@
 '''
-Algoritmo QuickSort com Particionamento Lomuto
+Algoritmo QuickSort com Pivot do Primeiro Elemento
 Adaptado do material em https://joaoarthurbm.github.io/eda/posts/quick-sort/
 '''
 
@@ -11,14 +11,23 @@ def trocarPosicao(lista, indiceI, indiceJ):
 def particao(lista, esquerda, direita):
     pivot = lista[esquerda]
     fronteira = esquerda
-    for i in range(esquerda,direita+1):
+    for i in range(esquerda+1,direita+1):
         if(lista[i] <= pivot):
             fronteira += 1
             trocarPosicao(lista,i,fronteira)
     trocarPosicao(lista,esquerda,fronteira)
+    return fronteira
 
 def quickSort(lista,esquerda,direita):
     if(esquerda < direita):
         indexPivot = particao(lista,esquerda,direita)
         quickSort(lista,esquerda, indexPivot - 1)
         quickSort(lista,indexPivot+1,direita)
+
+if __name__ == "__main__":
+    #lista = [3,2,8,1,5]
+    lista = [3,2,8,1,5]
+    n = len(lista)
+    print("Lista original:", lista)
+    quickSort(lista, 0, n-1)
+    print("Lista ordenada:", lista)  
